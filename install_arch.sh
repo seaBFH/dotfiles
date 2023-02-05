9bf9b0ee7f2bf4e911667027e627972055fa5ee8
Sea Liver <kakitree@yeah.net>
2023-02-05 22:09:59 +0800

    feat: arch package install

---
+++ install_arch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+# pacman -Syu
+pacman -Syu
+
+# sys tools
+pacman -S usbutils network-manager bluez bluez-utils konscole pulseaudio-bluetooth
+
+# pacman -S
+pacman -S stow nodejs yarn neovim fzf ranger lf paco exa ghq xclip kubectl helm openvpn qopenvpn bat curlie mysql-workbench containerd docker fuse2 fuse-overlays flameshot jq pandoc npm
+
+# npm install
+npm install -g live-server
+
+# aur
+yay -S ttf-victor-mono-nerd
+
+# rustup
+
+# go env
+
+# git global config
+
+# tmux tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
